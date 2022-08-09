67567e6bea8801bb02a7d1f22e076a7ba4cae771
Mehdi-Javadzadeh <75339855+Mehdi-Javadzadeh@users.noreply.github.com>
2022-08-10 01:45:09 +0430

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import * as React from "react";
+import { appWithTranslation } from "next-i18next";
+import dynamic from "next/dynamic";
+import Head from "next/head";
+import "../styles/globals.css";
+import { createGlobalStyle } from "styled-components";
+import "../public/fonts/font.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default dynamic(() => Promise.resolve(appWithTranslation(MyApp)), {
+  ssr: false,
+});
+
+const GlobalStyle = createGlobalStyle`
+*{
+  font-family : 'IRANSans' !important;
+}
+`;
