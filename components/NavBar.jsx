6a1e5f49d76aeb664ef76cf80b56735e8753ea14
Mehdi-Javadzadeh <75339855+Mehdi-Javadzadeh@users.noreply.github.com>
2022-08-25 18:47:42 +0430

    update

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -154,7 +154,7 @@ const NavBar = () => {
 				<div
 					className={
 						nav
-							? "md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 pb-0 ease-in duration-500"
+							? "md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10  ease-in duration-500"
 							: "fixed left-[-200%] top-0 p-10 ease-in duration-500"
 					}
 				>
--- components/PopupVideo.jsx
+++ components/PopupVideo.jsx
@@ -19,13 +19,13 @@ const PopupVideo = () => {
 			<div
 				className={
 					isPopUp
-						? " z-[200] fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-[90%] sm:w-[80%] lg:w-[60%] md:w-[70%] h-max bg-[#ecf0f3] p-6 ease-in duration-[850ms] rounded-2xl shadow-xl shadow-gray-800"
-						: "z-[200] fixed left-[-200%] bottom-0 w-[90%] sm:w-[80%] lg:w-[60%] md:w-[70%] h-max bg-[#fff] p-6 ease-in duration-[900ms] rounded-xl opacity-50 animate-ping"
+						? " z-[150] fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-[90%] sm:w-[80%] lg:w-[60%] md:w-[70%] h-max bg-[#ecf0f3] p-6 ease-in duration-500 rounded-2xl shadow-xl shadow-gray-800"
+						: "z-[150] fixed left-[-200%] bottom-0 bg-[#fff] p-6 ease-in duration-500 rounded-xl opacity-50 animate-ping"
 				}
 			>
 				<div className="w-full h-full flex flex-col items-center justify-center relative gap-5">
 					<div className="w-full flex items-center justify-between pl-10">
-						<p className="text-lg">Application Flow Video:</p>
+						<p className="text-base">Application Flow Video:</p>
 						<div className="cursor-pointer p-2 bg-[#60a5fa]/60 rounded-full ease-in duration-300">
 							<AiOutlineClose onClick={handleClose} size={30} color="#fff" />
 						</div>
